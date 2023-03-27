5cd2f969580871e9a14835940f00ee0536c5e0fb
Habibou Moussa <habibou997@gmail.com>
2023-03-27 14:51:28 +0000

    First endpoint (/index)

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+@api_view(['GET'])
+def index(request):
+    courses = {
+        'course_name' : 'Python',
+        'learn' : ['flask' , 'Django', 'Tornado' , 'FastApi'],
+        'course_provider' : 'habibou'
+    }
+    return Response(courses)
